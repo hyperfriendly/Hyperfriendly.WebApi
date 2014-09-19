182131e003abba716abe80f21ff8f5c688ed09ad
104171oyfa <oyvind.fanebust@frende.no>
2014-09-19 15:52:47 +0200

    Added error example

---
+++ Hyperfriendly.WebApi.Example/FailingController.cs
@@ -0,0 +1,16 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Hyperfriendly.WebApi.Example
+{
+    public class FailingController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            var errorResource = new ErrorResource();
+            errorResource.Errors.Add(new ErrorEntryResource("Bad input!", "Something failed because of bad input!"));
+            return Request.CreateResponse(HttpStatusCode.BadRequest, errorResource);
+        }
+    }
+}
